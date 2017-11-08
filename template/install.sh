c6ec17d7f9657b1583c71f53ff75cfba89648ec9
Ivan <ivan@youzi.com.cn>
2017-11-08 16:45:06 +0800

    发布

--- template/install.sh
+++ template/install.sh
@@ -31,6 +31,54 @@ REMOVE_RESOURCE=
 #       函数
 #
 ############################################################
+download()
+{
+        local DIR="$1"
+        local URL1="$2"
+        local URL2="$3"
+        local FILE=
+        local FILE_PATH=
+        local NUM=
+        
+        mkdir -p ${DIR}
+        
+        for FILE in "$@"; do
+                
+                if [[ "$FILE" == "$1" ]] || [[ "$FILE" == "$2" ]] || [[ "$FILE" == "$3" ]]; then
+                        continue 1
+                fi
+                
+                FILE_PATH="${DIR}/${FILE}"
+                
+                if [[ -f "$FILE_PATH" ]]; then
+                        rm -rf "$FILE_PATH"
+                fi
+                
+                for (( NUM=0; NUM < 10; NUM++ )); do
+                        
+                        if [[ -s "$FILE_PATH" ]] \
+                        && (  ! grep "^<title>" "$FILE_PATH" &> /dev/null ) \
+                        && (  ! grep "^404" "$FILE_PATH" &> /dev/null ); then
+                                break 1
+                        else
+                                curl -sSo "$FILE_PATH" ${URL1}/${URL2}/${FILE}
+                        fi
+                        
+                done
+                
+                if [[ "$NUM" == "10" ]]; then
+                        echo "Error: ${FILE} download failed"
+                        exit 1
+                fi
+                
+                if [[ "$DIR" == "/usr/local/bin" ]]; then
+                        chmod 755 "$FILE_PATH"
+                fi
+                
+                echo "download: ${FILE}"
+                
+        done
+}
 
 ############################################################
 #
