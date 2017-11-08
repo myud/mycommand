67b9cd5137ea3041469eebb5a0c87018a92447b6
Ivan <ivan@youzi.com.cn>
2017-11-08 23:43:36 +0800

    发布

---
+++ template/EOF.sh
@@ -0,0 +1,17 @@
+# EOF >> EOFS >> EOFT >> EOFF
+
+cat > / <<-"EOF"
+#!/bin/bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+set -eu
+
+# Variable
+
+# Function
+
+# Operation
+
+# End
+EOF
